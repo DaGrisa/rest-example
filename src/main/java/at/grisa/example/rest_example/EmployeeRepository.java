2ea6f9953078b867500d5faae890933a846236ab
daniel <dgr7348@students.fhv.at>
2016-11-18 22:18:05 +0100

    qwuery and docker

--- src/main/java/at/grisa/example/rest_example/EmployeeRepository.java
+++ src/main/java/at/grisa/example/rest_example/EmployeeRepository.java
@@ -1,7 +1,10 @@
 package at.grisa.example.rest_example;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-
+	List<Employee> findByLastNameIgnoreCase(@Param("lastName") String lastName);
 }
